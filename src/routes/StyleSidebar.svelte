0cf32fcb8fc583b06c853f9280523b4cc02221af
KevinWh0 <45321184+KevinWh0@users.noreply.github.com>
2022-12-12 23:34:56 -0800

    Start of styler buttons

--- src/routes/StyleSidebar.svelte
+++ src/routes/StyleSidebar.svelte
@@ -1,9 +1,54 @@
 <script lang="ts">
-	import { Label, Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+	import {
+		Badge,
+		Button,
+		ButtonGroup,
+		Card,
+		Label,
+		Sidebar,
+		SidebarGroup,
+		SidebarItem,
+		SidebarWrapper
+	} from 'flowbite-svelte';
 
 	let spanClass = 'flex-1 ml-3 whitespace-nowrap';
 </script>
 
 <Sidebar asideClass="w-full">
-	<Label class="text-center text-3xl">Styler</Label>
+	<!-- <Badge dismissable on:click={() => console.log('clicked')}>Dismissable</Badge> -->
+	<!-- <StyleTag
+		onClickFunc={() => {
+			console.log('hi');
+		}}
+		dismissable>Hello</StyleTag
+	> -->
+	<!-- <Card class="w-full p-6" padding="lg"> -->
+	<div
+		class="flex w-full flex-col rounded-lg border border-gray-200 bg-white p-4 text-gray-500 shadow-md dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 sm:p-6"
+	>
+		<Label class="pb-6 text-center text-3xl">Styler</Label>
+
+		<div class="flex w-full items-center justify-center">
+			<ButtonGroup divClass="max-w-full overflow-x-auto whitespace-nowrap" size="sm">
+				<Button outline>Default</Button>
+				<Button>1</Button>
+				<Button>2</Button>
+				<Button>3</Button>
+				<Button>4</Button>
+				<Button>5</Button>
+				<Button>6</Button>
+				<Button>7</Button>
+				<Button>8</Button>
+				<Button>9</Button>
+
+				<Button
+					on:click={() => {
+						console.log('hi');
+					}}>+</Button
+				>
+			</ButtonGroup>
+		</div>
+	</div>
+	<!-- </Card
+	> -->
 </Sidebar>
---
+++ src/routes/styleManager.ts
@@ -0,0 +1,118 @@
+//All the users style data
+// eslint-disable-next-line prefer-const
+export let styleData = [];
+//This is the style that they are currently editing.
+// eslint-disable-next-line prefer-const
+export let selectedStyle = 1;
+
+//Font dictionary taken from: https://github.com/arcusmaximus/YTSubConverter/blob/3455abe39138348283bc12f9ad2f5b94e2115e61/YTSubConverter/Formats/YttDocument.cs#L1173
+export const fontDictionary = {
+	'-1': 'Default',
+	1: 'Courier New', //works
+	2: 'Times New Roman', //works
+	3: 'Lucida Console', // Because of the incorrect "Deja Vu Sans Mono" font name in YouTube's captions.js, browsers fall back to this second option
+	5: 'Comic Sans Ms', // works
+	6: 'Monotype Corsiva', //seems to not work
+	7: 'Carrois Gothic Sc',
+	_: 'Roboto'
+};
+
+//Unused, might be used later if we add position support
+export const positionDictornary = {
+	BottomLeft: 1,
+	BottomCenter: 2,
+	BottomRight: 3,
+	MiddleLeft: 4,
+	Center: 5,
+	MiddleRight: 6,
+	TopLeft: 7,
+	TopCenter: 8,
+	TopRight: 9
+};
+
+//all the styles that can be applied and what input type they will need to be modified along with some perams
+const styleTypes = {
+	backgroundColor: 'color',
+	backgroundOpacity: 'numberRanged 0-255',
+	color: 'color',
+	italics: 'boolean',
+	bold: 'boolean',
+	underline: 'boolean',
+	font: 'dropdown ' + JSON.stringify(fontDictionary),
+	size: 'number 100',
+	fontOpacity: 'numberRanged 0-255',
+	'Dropshadow Color': 'color',
+	'Dropshadow Distance': 'numberRanged 1-4'
+};
+//This is a table for convering the data into the correct varible names for youtube
+export const styleToXML = {
+	backgroundColor: 'bc',
+	backgroundOpacity: 'bo',
+	color: 'fc',
+	italics: 'i',
+	bold: 'b',
+	underline: 'u',
+	font: 'fs',
+	size: 'sz',
+	fontOpacity: 'fo',
+	id: 'id',
+	'Dropshadow Color': 'ec',
+	'Dropshadow Distance': 'et'
+};
+
+//This is used for the preview
+export const stylePreview = {
+	backgroundColor: {
+		cssName: 'background-color',
+		value: 'INPUT',
+		effectedElement: 'previewText'
+	},
+	backgroundOpacity: {
+		cssName: 'btraparency',
+		value: 'INPUT',
+		effectedElement: 'previewText'
+	},
+	color: { cssName: 'color', value: 'INPUT', effectedElement: 'previewText' },
+	italics: {
+		cssName: 'font-style',
+		value: `INPUT == 1 ? "italic" : "normal"`,
+		effectedElement: 'previewText'
+	},
+	bold: {
+		cssName: 'font-weight',
+		value: `INPUT == 1 ? "bold" : "normal"`,
+		effectedElement: 'previewText'
+	},
+	underline: {
+		cssName: 'text-decoration',
+		value: `INPUT == 1 ? "underline" : "normal"`,
+		effectedElement: 'previewText'
+	},
+	font: {
+		cssName: 'font-family',
+		value: 'fontDictionary[INPUT]',
+		effectedElement: 'previewText'
+	},
+	size: {
+		cssName: 'font-size',
+		value: 'INPUT%',
+		effectedElement: 'previewText'
+	},
+
+	fontOpacity: {
+		cssName: 'traparency',
+		value: 'INPUT',
+		effectedElement: 'previewText'
+	},
+	id: null,
+	'Dropshadow Color': {
+		cssName: 'text-shadow',
+		value: 'INPUT',
+		effectedElement: 'previewText'
+	},
+	'Dropshadow Distance': {
+		cssName: 'text-shadow',
+		value: 'INPUT',
+		effectedElement: 'previewText'
+	}
+};
